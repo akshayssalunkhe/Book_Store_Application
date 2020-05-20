f74679a46ba2fe2c6004a427a9a88318b3007b2b
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-05-20 18:17:18 +0530

    ADDED:Project

---
+++ src/main/java/com/bookdemo/BookStoreAppApplication.java
@@ -0,0 +1,17 @@
+package com.bookdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreAppApplication.class, args);
+
+ //printing welcome message
+        System.out.println("Hello Akshay");
+    }
+
+ 
+}
